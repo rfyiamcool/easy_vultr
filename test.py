3ce95fa49694736b2092e2d16829e2f688cc7c2e
rfyiamcool <rfyiamcool@163.com>
2017-11-05 22:50:07 +0800

    add

---
+++ test.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import sys
+import logging
+from os import environ
+
+from vultr import Vultr
+
+SUB_ID = 11458384
+API_KEY = ''
+if not API_KEY:
+    API_KEY = environ.get('VULTR_KEY')
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s %(levelname)s [%(funcName)s():%(lineno)d] %(message)s'
+)
+logging.getLogger("requests").setLevel(logging.WARNING)
+
+
+def stop():
+    vultr = Vultr(API_KEY)
+    vultr.server.halt(SUB_ID)
+
+
+def start():
+    vultr = Vultr(API_KEY)
+    vultr.server.start(SUB_ID)
+
+
+if __name__ == "__main__":
+    if sys.argv[1] == "start":
+        start()
+    elif sys.argv[1] == "stop":
+        stop()
